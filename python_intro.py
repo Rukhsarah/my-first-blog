47e4737fa084a528c2ce81a2a3e26240d4889694
Rukhsora Musaeva <sarah579@mail.ru>
2017-11-26 15:52:38 +0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+def hello(name):
+    if name == 'Rukhsora':
+        print('Hi Rukhsora')
+    elif name == 'Sarah':
+        print('Hi Sarah')
+    else:
+        print('Hi Sam!')
+hello('Sarah')
+
+def hello():
+        print('Hi, how are you!?')
+        print('How is your studies?')
+
+hello()
+def hello(name):
+    print('Hi' + name + '!')
+
+girls = ['Marina', 'Sarah', 'Melania', 'Yana']
+for name in girls:
+    hello(name)
+    print('Next girl')
+
